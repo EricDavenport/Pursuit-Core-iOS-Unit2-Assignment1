2d7880cdff368f891d82f4b95f6fa346d39d3316
Eric Davenport <EricDavenport@pursuit.org>
2019-11-06 19:07:47 -0500

    Buttons added - action and location

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
+    
+    
+    @IBOutlet var gameButtons: [GameButton]!
+    
+    
+    
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var winningLabel: UILabel!
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-
+    @IBAction func gameButtonPressed(_ gameButton: GameButton) {
+        
+        print("ROW: \(gameButton.row)\nCOL: \(gameButton.col)")
+    }
+    
 }
 
