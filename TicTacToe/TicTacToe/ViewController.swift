3136587213bf07d54f1ca4b315bdc240796a1eed
Eric Davenport <EricDavenport@pursuit.org>
2019-11-07 17:48:13 -0500

    new game added - player wins count added but counting incorrectly

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -17,13 +17,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var newGameButton: UIButton!
     @IBOutlet weak var winningLabel: UILabel!
     
-
+    @IBOutlet weak var playerOneWinLabel: UILabel!
+    
+    @IBOutlet weak var playerTwoWinLabel: UILabel!
+    
     var gameStart = TicTacToeBrain()
     
     var gameRunning = true
     var player = 1
-    var playerOneMoves = [Int]()
-    var playerTwoMoves = [Int]()
+    var playerOneWins: Int = 0
+    var playerTwoWins: Int = 0
     
     
     
@@ -39,19 +42,19 @@ class ViewController: UIViewController {
         
         
         print("ROW: \(gameButton.row) COL: \(gameButton.col)")
-       // gameStart.playAction(gameButton)
+       
         let currentSpot = gameButton.tag
        
         if  player == 1 {
                    gameButton.setBackgroundImage(UIImage(named: "xmark"), for: UIControl.State.normal)
                    print("\(player)")
-            playerOneMoves.append(currentSpot)
+            
             gameStart.emptyAr[currentSpot] = player
                    player = 2
             gameButton.isEnabled = false
                } else {
                    gameButton.setBackgroundImage(UIImage(named: "circle"), for: UIControl.State.normal)
-            playerTwoMoves.append(currentSpot)
+            
             gameStart.emptyAr[currentSpot] = player
                    player = 1
                    print("\(player)")
@@ -62,15 +65,26 @@ class ViewController: UIViewController {
         
         winnerCheck()
     }
+    //MARK: WINNER CHECK
     func winnerCheck() {
         for each in gameStart.winningChances {
-            //for eachOne in each {
             if gameStart.emptyAr[each[0]] == 1 && gameStart.emptyAr[each[1]] == 1 && gameStart.emptyAr[each[2]] == 1 {
+                playerOneWins += 1
                     winningLabel.text = "Player one wins"
+                playerOneWinLabel.text = "\(playerOneWins)"
                 } else if gameStart.emptyAr[each[0]] == 2 && gameStart.emptyAr[each[1]] == 2 && gameStart.emptyAr[each[2]] == 2 {
+                playerTwoWins += 1
                     winningLabel.text = "Player two wins"
+                playerTwoWinLabel.text = "\(playerTwoWins)"
                 }
-        //}
     }
 }
+    //MARK: NEW GAME START
+    
+    @IBAction func newGameButtom(_ sender: UIButton) {
+        for eachButton in gameButtons {
+            eachButton.setBackgroundImage(nil, for: UIControl.State.normal)
+            eachButton.isEnabled = true
+        }
+    }
 }
